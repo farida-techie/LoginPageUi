c9fcd0dfdde58a4964b52b27e95dbe7623ccb294
farida-techie <fairdabhairakdar@gmail.com>
2021-07-11 17:56:01 +0530

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+j
+:app/src/main/java/com/malkinfo/loginpageui/MainActivity.kt,4/4/4493cc8552c882f2872037db1ed481099aac7aba
+i
+9app/src/main/java/com/malkinfo/loginpageui/NewActivity.kt,4/c/4c193d1f637abe77a7fc20769dc978a3aaf6be4d
---
+++ app/src/main/java/com/malkinfo/loginpageui/NewActivity.kt
@@ -0,0 +1,11 @@
+package com.malkinfo.loginpageui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
